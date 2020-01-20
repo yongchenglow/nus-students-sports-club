712f14079cb0f4c27be82358166e51c7918b2ee3
Yong Cheng Low <lowyongcheng@hotmail.com>
2020-01-20 20:53:32 +0800

    create site header

--- src/components/main/header.js
+++ src/components/main/header.js
@@ -7,22 +7,20 @@ class Header extends Component {
       <Helmet>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>My Title</title>
+        <title>NUS Students' Sports Club</title>
         <link rel="canonical" href="http://mysite.com/example" />
-
 
         <link rel="icon" href="img/logos/NUS_Biathlon_Swim_Run.png">
         <title>NUS Students' Sports Club</title>
-        <meta name="author" content="NUS Biathlon | NUS Students' Sports Club">
-        <meta name="description" content="NUS Biathlon is the most anticipated annual swim and run event to promote healthy lifestyle">
-        <meta name="keywords" content="NUS Biathlon, NUS Aquathlon, Swim & Run, NUSSSC, NUS Students' Sports Club">
-
+        <meta name="author" content="NUS Students' Sports Club">
+        <meta name="description" content="NUS Students' Sports Club is the leading platform in promoting community bonding through recreational sports in NUS.">
+        <meta name="keywords" content="NUS Students' Sports Club, NUSSSC, NUS Sports, Sports, NUS Sports Club, National University of Singapore, NUS">
 
         <meta property="og:type" content="website">
-        <meta property="og:url" content="https://nussportsclub.org/nusbiathlon">
-        <meta property="og:image" content="img/logos/NUS_Biathlon_2020.png">
-        <meta property="og:title" content="NUS Biathlon | NUS Students' Sports Club">
-        <meta property="og:description" content="NUS Biathlon is the most anticipated annual swim and run event to promote healthy lifestyle">
+        <meta property="og:url" content="https://www.nussportsclub.org/">
+        <meta property="og:image" content="img/logos/Sports_Club_Logo.png">
+        <meta property="og:title" content="NUS Students' Sports Club">
+        <meta property="og:description" content="NUS Students' Sports Club is the leading platform in promoting community bonding through recreational sports in NUS.">
       </Helmet>
     );
   }
