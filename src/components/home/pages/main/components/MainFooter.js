da018dbfabfe3fd8cf5e65a2304e4eec92843db4
Yong Cheng Low <lowyongcheng@hotmail.com>
2020-02-10 16:14:53 +0800

    Added Font Awesome and Adjust Page footer

--- src/components/home/pages/main/components/MainFooter.js
+++ src/components/home/pages/main/components/MainFooter.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faTelegramPlane } from '@fortawesome/free-brands-svg-icons';
 
 class MainFooter extends Component {
   render () {
     return (
       <div className='footer'>
         <Container>
-          <Row className='justify-content-center'>
-            <Col xs={12} sm={8} md={6} lg={5} xl={4}>
-               <div class="copyright text-white">Copyright &copy; NUS Students' Sports Club 2020</div>
+          <Row>
+            <Col className='text-left'>
+              <div className='footer-text'><strong>Address:</strong> 8 Sports Drive 1 S(117564)</div>
+              <div className='footer-text'><strong>Email:</strong> enquiry@nussportsclub.org</div>
+            </Col>
+            <Col>
+              <Button variant='outline-light' className='social-button' href='https://www.facebook.com/NUSSportsClub/'>
+                <FontAwesomeIcon icon={faFacebookF}/>
+              </Button>
+              <Button variant='outline-light' className='social-button' style={{ fontSize: '1.8rem', padding: '0.15rem' }} href='https://www.instagram.com/nussportsclub/?hl=en'>
+                <FontAwesomeIcon icon={faInstagram}/>
+              </Button>
+              <Button variant='outline-light' className='social-button' style={{ fontSize: '1.7rem', padding: '0.2rem' }} href='https://t.me/NUSSportsClub'>
+                <FontAwesomeIcon icon={faTelegramPlane}/>
+              </Button>
+            </Col>
+            <Col>
+              <small className='copyright'>&copy; Copyright 2020 NUS Students' Sports Club <br/> All Rights Reserved</small>
             </Col>
           </Row>
         </Container>
