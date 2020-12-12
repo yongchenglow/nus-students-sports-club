6ec73e0a4fc1d59e48e17e58b301a7628651ef87
Yong Cheng Low <lowyongcheng@hotmail.com>
2020-12-12 15:51:54 +0800

    removed popover for runnus and vs sign ups. ajusted partners

--- src/components/home/pages/main/components/body/partners/Partners.js
+++ src/components/home/pages/main/components/body/partners/Partners.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Cover from './components/Cover';
+import OurPartners from './components/OurPartners';
 
 class Partners extends Component {
   render () {
     return (
       <React.Fragment>
         <Cover />
+        <OurPartners />
       </React.Fragment>
     );
   }
