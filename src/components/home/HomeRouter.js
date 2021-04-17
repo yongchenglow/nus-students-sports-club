de237adda3391faaacb06e5a27349eb5f05134e3
Yong Cheng Low <lowyongcheng@hotmail.com>
2021-04-17 15:17:45 +0800

    [Enhancement] Adjusted SunNUS to redirect to SunNUS 2021 website

--- src/components/home/HomeRouter.js
+++ src/components/home/HomeRouter.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
-import { Route, Switch} from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
 // Components
-import Main from './pages/main/Main';
-import NUSBiathlon from './pages/nusbiathlon/NUSBiathlon';
-import RunNUS from './pages/runnus/RunNUS';
-import SunNUS from './pages/sunnus/SunNUS';
-import InterFacultyGames from './pages/ifg/InterFacultyGames';
+import Main from "./pages/main/Main";
+import NUSBiathlon from "./pages/nusbiathlon/NUSBiathlon";
+import RunNUS from "./pages/runnus/RunNUS";
+import SunNUS from "./pages/sunnus/SunNUS";
+import InterFacultyGames from "./pages/ifg/InterFacultyGames";
 
 class MainRouter extends Component {
-  render(){
+  render() {
     return (
       <React.Fragment>
         <Switch>
-          <Route path='/nusbiathlon' exact component={NUSBiathlon} />
-          <Route path='/runnus' exact component={RunNUS} />
-          <Route path='/sunnus' exact component={SunNUS} />
-          <Route path='/ifg' exact component={InterFacultyGames} />
-          <Route path='/' component={Main} />
+          <Route path="/nusbiathlon" exact component={NUSBiathlon} />
+          <Route path="/runnus" exact component={RunNUS} />
+          <Route
+            path="/sunnus"
+            exact
+            component={() => {
+              window.location.href = "https://www.sunnus2021.nussportsclub.org";
+              return null;
+            }}
+          />
+          <Route path="/ifg" exact component={InterFacultyGames} />
+          <Route path="/" component={Main} />
         </Switch>
       </React.Fragment>
     );
