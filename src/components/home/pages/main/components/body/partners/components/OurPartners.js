f6e9103aaff5a4ebb050f4489e3530473d58d14a
yong cheng low <lowyongcheng@hotmail.com>
2021-08-08 01:11:10 +0800

    [Enhancement] Made Partners Adjustable

--- src/components/home/pages/main/components/body/partners/components/OurPartners.js
+++ src/components/home/pages/main/components/body/partners/components/OurPartners.js
@@ -1,116 +1,85 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col, Container } from "react-bootstrap";
+import { Redirect } from "react-router-dom";
 
-import ariston from "../images/ariston.png";
-import mc2 from "../images/mc2.jpg";
-import nivea from "../images/nivea.jpg";
-import psFoodAndBeverage from "../images/psFoodAndBeverage.png";
-import SengHong from "../images/SengHong.jpg";
-import Unilever from "../images/Unilever.bmp";
-import Win2 from "../images/Win2.jpg";
-import wipro from "../images/wipro.png";
-import AF from "../images/af.jpg";
-import Cationorm from "../images/cationorm.png";
-import MyProteih from "../images/myproteih.jpeg";
-import Rev from "../images/rev.jpg";
+function OurPartners() {
+  const [partners, setPartners] = useState();
 
-class OurPartners extends Component {
-  render() {
-    return (
-      <div className="section text-center">
-        <Container>
-          <div className="section-heading">Featuring</div>
-          <Row className="justify-content-center mt-2 h-100">
-            <Col sm={3} className="my-auto pb-4">
-              <img
-                className="img-fluid partner-img-high"
-                src={nivea}
-                alt="Nivea"
-              />
-            </Col>
-            <Col sm={3} className="my-auto pb-4">
-              <img
-                className="img-fluid partner-img-high"
-                src={Unilever}
-                alt="Unilever"
-              />
-            </Col>
-            <Col sm={3} className="my-auto pb-4">
-              <img
-                className="img-fluid partner-img-high"
-                src={wipro}
-                alt="wirpo"
-              />
-            </Col>
-            <Col sm={3} className="my-auto pb-4">
-              <img
-                className="img-fluid partner-img-high"
-                src={psFoodAndBeverage}
-                alt="psFoodAndBeverage"
-              />
-            </Col>
-          </Row>
-          <Row className="justify-content-center h-100">
-            <Col sm={2} className="my-auto pb-4">
-              <img className="img-fluid partner-img-medium" src={AF} alt="AF" />
-            </Col>
-            <Col sm={2} className="my-auto pb-4">
-              <img
-                className="img-fluid partner-img-medium"
-                src={Cationorm}
-                alt="Cationorm"
-              />
-            </Col>
-            <Col sm={2} className="my-auto pb-4">
-              <img
-                className="img-fluid partner-img-medium"
-                src={MyProteih}
-                alt="My Proteih"
-              />
-            </Col>
-            <Col sm={2} className="my-auto pb-4">
-              <img
-                className="img-fluid partner-img-medium ml-5 ml-m-0"
-                src={Rev}
-                alt="Rev"
-              />
-            </Col>
-            <Col sm={2} className="my-auto pb-4">
-              <img
-                className="img-fluid partner-img-medium"
-                src={SengHong}
-                alt="SengHong"
-              />
-            </Col>
-          </Row>
-          <Row className="justify-content-center h-100">
-            {/* <Col sm={2} className="my-auto pb-4">
-              <img
-                className="img-fluid partner-img-medium"
-                src={mc2}
-                alt="MC2"
-              />
-            </Col> */}
+  const [hasError, setError] = useState(false);
 
-            {/* <Col sm={2} className="my-auto pb-4">
-              <img
-                className="img-fluid partner-img-medium"
-                src={ariston}
-                alt="Ariston"
-              />
-            </Col> */}
-            {/* <Col sm={2} className="my-auto pb-4">
-              <img
-                className="img-fluid partner-img-medium ml-5 ml-m-0"
-                src={Win2}
-                alt="Win2"
-              />
-            </Col> */}
-          </Row>
-        </Container>
-      </div>
-    );
+  const getPartners = () => {
+    fetch(process.env.REACT_APP_41_PARTNERS_DATA_PATH, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson.partners);
+        setPartners(generatePartners(myJson.partners));
+      })
+      .catch(function () {
+        setError(true);
+      });
+  };
+
+  useEffect(() => {
+    getPartners();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (hasError === true) {
+    return <Redirect to="/notfound" />;
   }
+
+  const generatePartners = (partners) => {
+    return partners.map((partner) => {
+      let rows = partner.rows.map((row) => {
+        console.log(row);
+        let col = row.sponsors.map((sponsor) => {
+          console.log(sponsor);
+          let content = (
+            <Col sm={sponsor.size} className="my-auto pb-4">
+              <img
+                className="img-fluid partner-img-high"
+                src={
+                  process.env.REACT_APP_41_PARTNERS_IMAGE_PATH + sponsor.image
+                }
+                alt={sponsor.name}
+              />
+            </Col>
+          );
+          if (sponsor.image_link === "") {
+            return content;
+          } else {
+            return (
+              <a
+                href={sponsor.image_link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {content}
+              </a>
+            );
+          }
+        });
+        return <Row className="justify-content-center h-100">{col}</Row>;
+      });
+      return (
+        <div className="section text-center">
+          <Container>
+            <div className="section-heading mb-4">Featuring</div>
+            {rows}
+          </Container>
+        </div>
+      );
+    });
+  };
+
+  return <React.Fragment>{partners}</React.Fragment>;
 }
 
 export default OurPartners;
