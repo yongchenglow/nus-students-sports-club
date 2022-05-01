aa329783752dfa6e332f09056d0c048fb5e325d8
Yong Cheng Low <lowyongcheng@hotmail.com>
2022-05-01 09:10:19 +0800

    Routes: Adjust routes for ifg and sunnus

--- src/components/home/HomeRouter.js
+++ src/components/home/HomeRouter.js
@@ -5,8 +5,8 @@ import { Route, Switch } from "react-router-dom";
 import Main from "./pages/main/Main";
 import NUSBiathlon from "./pages/nusbiathlon/NUSBiathlon";
 import RunNUS from "./pages/runnus/RunNUS";
-import SunNUS from "./pages/sunnus/SunNUS";
-import InterFacultyGames from "./pages/ifg/InterFacultyGames";
+// import SunNUS from "./pages/sunnus/SunNUS";
+// import InterFacultyGames from "./pages/ifg/InterFacultyGames";
 
 class MainRouter extends Component {
   render() {
@@ -15,8 +15,22 @@ class MainRouter extends Component {
         <Switch>
           <Route path="/nusbiathlon" exact component={NUSBiathlon} />
           <Route path="/runnus" exact component={RunNUS} />
-          <Route path="/sunnus" exact component={SunNUS} />
-          <Route path="/ifg" exact component={InterFacultyGames} />
+          <Route
+            path="/sunnus"
+            exact
+            component={() => {
+              window.location.href = "https://www.sunnus.nussportsclub.org";
+              return null;
+            }}
+          />
+          <Route
+            path="/ifg"
+            exact
+            component={() => {
+              window.location.href = "https://www.ifg.nussportsclub.org";
+              return null;
+            }}
+          />
           <Route path="/" component={Main} />
         </Switch>
       </React.Fragment>
